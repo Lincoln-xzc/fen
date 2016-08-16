1b6e270bb47f01bd36e30e22ea8acb9dad055b03
Lincoln-xzc <xzc524813657@sina.com>
2016-08-16 20:03:39 +0800

    'update'

---
+++ app/midleware/topCarousel.js
@@ -0,0 +1,89 @@
+/**
+ * Created by Htmler on 2016/8/16.
+ */
+import React from 'react';
+import {render} from 'react-dom';
+
+export default  class TopCarousel extends React.Component {
+    render() {
+        return (
+            <div className="dyright fr rel">
+                <div  className="abs">
+                    <ul>
+                        <li>
+                            <div className="news"> <img src="/app/images/6.jpg" className="fl newspic" style={{width:142+'px',height:125+'px'}} data-bd-imgshare-binded="1"/> <span >经营禾谷渔粉店如何实现快速盈利</span>
+                                <p>禾谷渔粉店投资经营有妙招，想实现快速盈利，诚信是禾谷渔粉品牌长久经营的根本，也是做人的一个品质体现。
+                                </p>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="news"> <img src="/app/images/6.jpg" className="fl newspic" style={{width:142+'px',height:125+'px'}} data-bd-imgshare-binded="1"/> <span >禾谷渔粉长春展会现场</span>
+                                <p>消费者的认可！ 客户的信赖！我们将最优质的最健康的产品提供给消费者；我们将更专业的服务提供给我们的客户；没错~这就是我们正在努力做的。</p>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="news"> <img src="/app/images/6.jpg" className="fl newspic" style={{width:142+'px',height:125+'px'}} data-bd-imgshare-binded="1"/> <span>吃货来跟你聊聊禾谷渔粉鱼汤的奥秘</span>
+                                <p>修养身心，滋阴补阳，禾谷渔粉教您养生之道。</p>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="news"> <img src="/app/images/6.jpg" className="fl newspic" style={{width:142+'px',height:125+'px'}} data-bd-imgshare-binded="1"/> <span>吃货来跟你聊聊禾谷渔粉鱼汤的奥秘</span>
+                                <p>目前，中式快餐店正在逐步取代西式快餐店，中餐毕竟还是中国人自己的食物，人们也更加喜欢中餐。在高节奏的城市生活中，时间和效率就是金钱，而中式快餐店正是凭借时间少，高效率获得成功，拥有无限商机!</p>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="news"> <img src="/app/images/6.jpg" className="fl newspic" style={{width:142+'px',height:125+'px'}} data-bd-imgshare-binded="1"/> <span>吃货来跟你聊聊禾谷渔粉鱼汤的奥秘</span>
+                                <p>2015年11月14-15日在北京全国农业展览馆举办的“2015第27届北京国际连锁加盟展览会”</p>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="news"> <img src="/app/images/6.jpg" className="fl newspic" style={{width:142+'px',height:125+'px'}} data-bd-imgshare-binded="1"/> <span>吃货来跟你聊聊禾谷渔粉鱼汤的奥秘</span>
+                                <p>2015年9月3日-9月5日在长春全国餐饮展览馆举办的“2015第16届长春国际连锁加盟展览会”</p>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="news"> <img src="/app/images/6.jpg" className="fl newspic" style={{width:142+'px',height:125+'px'}} data-bd-imgshare-binded="1"/> <span>吃货来跟你聊聊禾谷渔粉鱼汤的奥秘</span>
+                                <p>修养身心，滋阴补阳，禾谷渔粉教您养生之道。</p>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="news"> <img src="/app/images/6.jpg" className="fl newspic" style={{width:142+'px',height:125+'px'}} data-bd-imgshare-binded="1"/> <span>吃货来跟你聊聊禾谷渔粉鱼汤的奥秘</span>
+                                <p>目前，中式快餐店正在逐步取代西式快餐店，中餐毕竟还是中国人自己的食物，人们也更加喜欢中餐。在高节奏的城市生活中，时间和效率就是金钱，而中式快餐店正是凭借时间少，高效率获得成功，拥有无限商机!</p>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="news"> <img src="/app/images/6.jpg" className="fl newspic" style={{width:142+'px',height:125+'px'}} data-bd-imgshare-binded="1"/> <span>吃货来跟你聊聊禾谷渔粉鱼汤的奥秘</span>
+                                <p>2015年11月14-15日在北京全国农业展览馆举办的“2015第27届北京国际连锁加盟展览会”</p>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="news"> <img src="/app/images/6.jpg" className="fl newspic" style={{width:142+'px',height:125+'px'}} data-bd-imgshare-binded="1"/> <span>吃货来跟你聊聊禾谷渔粉鱼汤的奥秘</span>
+                                <p>2015年9月3日-9月5日在长春全国餐饮展览馆举办的“2015第16届长春国际连锁加盟展览会”</p>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="news"> <img src="/app/images/6.jpg" className="fl newspic" style={{width:142+'px',height:125+'px'}} data-bd-imgshare-binded="1"/> <span>吃货来跟你聊聊禾谷渔粉鱼汤的奥秘</span>
+                                <p>禾谷渔粉是以绿色的五谷杂粮做成，有小麦，小米，荞麦，高粱，大米做成，百分百不含添加剂及食用胶，纯天然养生食品。</p>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="news"> <img src="/app/images/6.jpg" className="fl newspic" style={{width:142+'px',height:125+'px'}} data-bd-imgshare-binded="1"/> <span>吃货来跟你聊聊禾谷渔粉鱼汤的奥秘</span>
+                                <p>禾谷渔粉的五谷杂粮保证健康、新鲜、养生的原则为大家打造健康的营养美食。</p>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="news"> <img src="/app/images/6.jpg" className="fl newspic" style={{width:142+'px',height:125+'px'}} data-bd-imgshare-binded="1"/><span>吃货来跟你聊聊禾谷渔粉鱼汤的奥秘</span>
+                                <p>禾谷渔粉制作的高汤放的是上等的新鲜鲫鱼，鲫鱼所含的蛋白质质优、齐全，容易消化吸收，是肝肾疾病、心脑血管疾病患者的良好蛋白质来源。</p>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="news"> <img src="/app/images/6.jpg" className="fl newspic" style={{width:142+'px',height:125+'px'}} data-bd-imgshare-binded="1"/> <span>吃货来跟你聊聊禾谷渔粉鱼汤的奥秘</span>
+                                <p>如今的社会竞争日益激烈，禾谷鱼粉想要在激烈的市场竞争中保证自己能够生存下去，就应该知道在开禾谷鱼粉时需要了解的事情。</p>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        )
+    }
+}
