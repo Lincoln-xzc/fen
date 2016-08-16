69f0259357329bb5f41ef2ab52069f0839702cd4
Lincoln-xzc <xzc524813657@sina.com>
2016-08-17 00:37:55 +0800

    'update'

--- app/js/index.js
+++ app/js/index.js
@@ -3,39 +3,6 @@
  */
 import React from 'react';
 import {render} from 'react-dom';
-<<<<<<< HEAD
-import Title from '../midleware/title.js';
-import {Carousel} from 'react-bootstrap';
-export default  class Index extends React.Component{
-    render(){
-        return (
-            <div className = ''>
-                <Title />
-                <Carousel>
-                    <Carousel.Item>
-                        <img width={900} height={500} alt="900x500" src="/assets/carousel.png"/>
-                        <Carousel.Caption>
-                            <h3>First slide label</h3>
-                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img width={900} height={500} alt="900x500" src="/assets/carousel.png"/>
-                        <Carousel.Caption>
-                            <h3>Second slide label</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img width={900} height={500} alt="900x500" src="/assets/carousel.png"/>
-                        <Carousel.Caption>
-                            <h3>Third slide label</h3>
-                            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                </Carousel>
-                <h3>Index</h3>
-=======
 import Menu from '../midleware/menu.js';
 import Footer from '../midleware/footer.js';
 export default  class Index extends React.Component{
@@ -45,7 +12,6 @@ export default  class Index extends React.Component{
                 <Menu />
                 {this.props.children}
                 <Footer />
->>>>>>> 1b6e270bb47f01bd36e30e22ea8acb9dad055b03
             </div>
         )
     }
