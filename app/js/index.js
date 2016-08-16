5d2b3aae2bb4716dc82f32090d693d2eb96330c0
Lincoln-xzc <xzc524813657@sina.com>
2016-08-16 22:00:10 +0800

    'update'

--- app/js/index.js
+++ app/js/index.js
@@ -4,12 +4,35 @@
 import React from 'react';
 import {render} from 'react-dom';
 import Title from '../midleware/title.js';
-
+import {Carousel} from 'react-bootstrap';
 export default  class Index extends React.Component{
     render(){
         return (
-            <div>
+            <div className = ''>
                 <Title />
+                <Carousel>
+                    <Carousel.Item>
+                        <img width={900} height={500} alt="900x500" src="/assets/carousel.png"/>
+                        <Carousel.Caption>
+                            <h3>First slide label</h3>
+                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img width={900} height={500} alt="900x500" src="/assets/carousel.png"/>
+                        <Carousel.Caption>
+                            <h3>Second slide label</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img width={900} height={500} alt="900x500" src="/assets/carousel.png"/>
+                        <Carousel.Caption>
+                            <h3>Third slide label</h3>
+                            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
                 <h3>Index</h3>
             </div>
         )
